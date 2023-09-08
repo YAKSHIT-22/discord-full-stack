1489f50a5cc42cc44a877f8b820dfa8feaaff955
YAKSHIT-22 <yakshitgarg25@gmail.com>
2023-09-08 11:03:03 +0530

    added server | invite | membersrole

---
+++ hooks/use-modal-store.ts
@@ -0,0 +1,28 @@
+import { Server } from "@prisma/client";
+import {create} from "zustand";
+
+
+export type ModalType = "createServer" | "invite" | "editServer" | "members";
+
+
+interface ModalData{
+  server?:Server
+}
+
+
+
+interface ModalStore {
+    type: ModalType | null;
+    data:ModalData;
+    isOpen: boolean;
+    onOpen: (type: ModalType, data?: ModalData) => void;
+    onClose: () => void;
+}
+
+export const useModalStore = create<ModalStore>((set) => ({
+    type: null,
+    data:{},
+    isOpen: false,
+    onOpen: (type, data={}) => set({isOpen: true, type: type, data:data}),
+    onClose: () => set({isOpen: false, type: null}),
+}));
